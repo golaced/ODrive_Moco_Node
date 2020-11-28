27085848042e14b7760c4d8d24be637e7d649b5b
Boyang Xing <golaced@163.com>
2020-11-28 09:36:07 +0800

    更新

---
+++ CAN_Demo/build/my_math.d
@@ -0,0 +1,2 @@
+.\build\my_math.o: applications\my_math.c
+.\build\my_math.o: applications\my_math.h
---
+++ CAN_Demo/build/mymath.d
@@ -0,0 +1,3 @@
+.\build\mymath.o: applications\mymath.c
+.\build\mymath.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\build\mymath.o: applications\mymath.h
